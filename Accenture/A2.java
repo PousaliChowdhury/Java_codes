bec0971aca36c661c19d6badef0743759ae01d6f
Pousali Chowdhury <136922307+PousaliChowdhury@users.noreply.github.com>
2024-08-23 21:19:30 +0530

    Add files via upload

---
+++ Accenture/A2.java
@@ -0,0 +1,35 @@
+package Accenture;
+
+import java.util.*;
+
+public class A2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        Stack<Integer> s = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] output = new int[n];
+
+        for(int i=n-1; i>=0; i--){
+            int x = arr[i];
+            if ((n==0))
+                return;
+            if(s.size()>0 && x<s.peek())
+                s.pop();
+             if(s.size() == 0)
+                output[i] = -1;
+            else if(s.size() > 0)
+                output[i] = s.peek();
+            s.push(x);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(output[i] + " ");
+        }
+        
+    }
+}
