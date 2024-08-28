8467f3f424064ff6836cd908c356d92be3fd7e0a
Pousali Chowdhury <136922307+PousaliChowdhury@users.noreply.github.com>
2024-08-28 14:38:17 +0530

    Add files via upload

---
+++ Accenture/A6.java
@@ -0,0 +1,14 @@
+// Hungry Sequence
+
+import java.util.Scanner;
+
+public class A6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.print(n+i + " ");
+        }
+    }
+}
---
+++ Accenture/A7.java
@@ -0,0 +1,17 @@
+// Find the value least distance from num and divisable by m
+
+import java.util.Scanner;
+
+public class A7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int m = sc.nextInt();
+        int r = num % m;
+
+        if(r<(m+1)/2)
+            System.out.println(num-r); //smaller value
+        else
+            System.out.println(num+m-r); //bigger value
+    }
+}
