ee3c84e1917bf801546b55a3cbc133384a92ed70
Pousali Chowdhury <136922307+PousaliChowdhury@users.noreply.github.com>
2025-02-12 21:22:53 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -16,3 +16,32 @@ public class Factorial {
         System.out.println(fact);
     }
 }
+
+
+//  if ignore -ve is mentioned and no factors for 0 then 
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();  
+
+        if (n == 0) { 
+            System.out.println("No Factors");
+            return;
+        }
+
+        int num = Math.abs(n);  // Ignore negative sign
+
+        System.out.print("Factors of " + n + " are: ");
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
+
